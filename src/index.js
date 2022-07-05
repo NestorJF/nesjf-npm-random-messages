ab0141584f04b8256681ff53c78b42264758979e
Nestor Flores <nesjfgon@gmail.com>
2022-07-04 20:04:48 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const messages = [
+    "Hello World",
+    "Hola Mundo",
+    "Bonjour le monde",
+    "Hallo Welt",
+    "Ciao mondo"
+];
+
+
+const randomMessage = () => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    console.log(messages[randomIndex]);
+}
+
+module.exports = { randomMessage };
